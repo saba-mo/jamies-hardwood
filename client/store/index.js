96885045b6c5c8973afb1af3bc87699a9198a8a9
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-12 14:24:39 -0800

    feat: created redux folder, usersreducer file, userlist file

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,31 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
+import axios from 'axios'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 
 const reducer = combineReducers({user})
+
+// this chunk was given to us in JPFP
+// let middleware = [
+//   // `withExtraArgument` gives us access to axios in our async action creators!
+//   // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
+//   thunkMiddleware.withExtraArgument({axios}),
+// ]
+// if (process.browser) {
+//   /*We'd like the redux logger to only log messages when it's running in the browser, and not when we run the tests from within Mocha. */
+//   middleware = [...middleware, createLogger({ collapsed: true })]
+// }
+// I think by adding `.withExtraArgument({axios})` below it doesn't change is set up in BoilerMaker and enables us to access axios in the same way
+
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(
+    thunkMiddleware.withExtraArgument({axios}),
+    createLogger({collapsed: true})
+  )
 )
+
 const store = createStore(reducer, middleware)
 
 export default store
---
+++ client/store/redux/users/usersReducer.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const SET_USERS = 'SET_USERS'
+const SET_A_USER = 'SET_A_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+// const defaultUser = {}
+// in JPFP started with below. users.js in BoilerMaker is above
+const initialState = []
+
+/**
+ * ACTION CREATORS
+ */
+export const destroyUser = user => ({
+  type: REMOVE_USER,
+  user: user
+})
+export const setUser = user => ({
+  type: SET_A_USER,
+  user: user
+})
+export const setUsers = users => ({
+  type: SET_USERS,
+  users: users
+})
+
+/**
+ * THUNK CREATORS
+ */
+
+export const deleteUser = user => async (dispatch, getState, {axios}) => {
+  try {
+    await axios.delete(`/api/users/${user.id}`)
+    dispatch(destroyUser(user))
+  } catch (error) {
+    console.error('could not delete user:', error)
+  }
+}
+
+export const fetchUser = userId => async (dispatch, getState, {axios}) => {
+  try {
+    const {data} = await axios.get(`/api/users/${userId}`)
+    dispatch(setUser(data))
+  } catch (error) {
+    console.log('unable to fetch user:', error)
+  }
+}
+
+export const fetchUsers = () => async (dispatch, getState, {axios}) => {
+  try {
+    const {data} = await axios.get(`/api/users/`)
+    dispatch(setUsers(data))
+  } catch (error) {
+    console.log('Unable to fetch all users:', error)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(users = initialState, action) {
+  switch (action.type) {
+    case DELETE_USER:
+      users = users.filter(
+        user => parseInt(user.id) !== parseInt(action.user.id)
+      )
+      return users
+    case SET_A_USER:
+      users = users.filter(
+        user => parseInt(user.id) !== parseInt(action.robot.id)
+      )
+      return users.concat([action.user])
+    case SET_USERS:
+      return action.users
+
+    default:
+      return users
+  }
+}
