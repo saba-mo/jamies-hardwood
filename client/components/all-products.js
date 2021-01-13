fd9c84e3a4932ff65bf167797b5fd5ebf56a0d7b
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-13 10:35:55 -0500

    updated all-products component to include image, price and name. Link might not work

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import Navbar from './navbar';
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import Navbar from './navbar'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
 
 export class AllProducts extends React.Component {
   componentDidMount() {
-    // this.props.getProjects();
+    this.props.getProducts()
   }
+
+  //is the link correct? Check
+  //Should clicking image OR title OR price take you to the product?
+  //with jsx, can I just throw html like $ in there?
 
   render() {
     return (
       <div>
-        <nav>
-          {Navbar}
-        </nav>
+        <nav>{Navbar}</nav>
         <div>
-          {this.props.products.map((project) => (
-            <div key={project.id}>
-              <Link to={`/projects/${project.id}`}>
-                <h3>{product.title}</h3>
+          {this.props.products.map((product) => (
+            <div key={product.id}>
+              <Link to={`/products/${product.id}`}>
+                <h2>{product.name}</h2>
+                <h4>${product.price}</h4>
+                <img src={product.imageUrl} />
               </Link>
             </div>
           ))}
