43160630d99ed708bed0aec30ca1c0679ad7c68c
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-12 20:26:39 -0800

    front end for userlist mostly done and inidividual user started

---
+++ client/components/users/UserList.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchUsers, removeUser} from '../../store/redux/robots/robotsReducer'
+import {Link} from 'react-router-dom'
+import User from './user'
+
+export class UserList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+  componentDidMount() {
+    this.props.loadUsers()
+  }
+
+  handleDelete = userToDelete => {
+    this.props.deleteUser(userToDelete)
+  }
+
+  render() {
+    const userList = this.props.users
+    return (
+      <div>
+        <main>
+          <h1>Jamie's Hard Wood and Heavy Metal </h1>
+          <h3>See all customers and admin users here.</h3>
+          <p />
+        </main>
+        <div id="full-list-display">
+          <div id="list">
+            <ul>
+              {userList.map(user => {
+                return (
+                  <div key={`user-${user.id}`}>
+                    <Link to={`/users/${user.id}`}>
+                      <h4>
+                        {user.firstname} {user.lastname}
+                      </h4>
+                    </Link>
+                    Click here to delete {user.firstname} {user.lastname} --->
+                    <button
+                      type="button"
+                      onClick={() => this.handleDelete(user)}
+                    >
+                      Delete
+                    </button>
+                    <User user={user} />
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {users: state.users}
+}
+
+const mapDispatchToProps = dispatch => ({
+  removeUser: user => dispatch(removeUser(user)),
+  loadUsers: () => dispatch(fetchUsers())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserList)
---
+++ client/components/users/user.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// user type is customer or admin
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user
+    return (
+      <div className="user-individual">
+        <p>
+          User name {user.firstname} {user.lastname}.
+        </p>
+        <p>User email {user.email}</p>
+        <p>User type {user.type}</p>
+      </div>
+    )
+  }
+}
+
+export default User
