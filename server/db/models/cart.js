27abeb6ffffae4fd34420d594d4e1759da94502f
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-01-14 08:44:38 -0800

    updated after PR comments

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,7 +4,6 @@ const db = require('../db')
 module.exports = db.define('cart', {
   quantity: {
     type: Sequelize.INTEGER,
-    // look into whether this the place to add max: item inventory
     validate: {
       min: 0
     }
