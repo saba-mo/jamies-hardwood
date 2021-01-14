4fe28528f52acaf5e363637a7bea55c93832a68f
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-14 17:58:13 -0500

    started navbar without cart component. Login/out doesn't work. Removed export from express-gate. Added Routes for navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {logout} from '../store';
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Jamie's Hardwood and Heavy Metal</h1>
     <nav>
-      {isLoggedIn ? (
+      {!isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/products">Shop</Link>
+          <Link to="/cart">Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -26,31 +28,31 @@ const Navbar = ({handleClick, isLoggedIn}) => (
     </nav>
     <hr />
   </div>
-)
+);
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
-  }
-}
+    isLoggedIn: !!state.user.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+      dispatch(logout());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+  isLoggedIn: PropTypes.bool.isRequired,
+};
--- server/express-gate-auth.js
+++ server/express-gate-auth.js
@@ -1,2 +1,3 @@
-export const isAdmin = (req, res, next) =>
+// export
+const isAdmin = (req, res, next) =>
   req.user.isAdmin ? next() : res.send("You don't have permission");
