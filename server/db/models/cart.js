57a1c368dc46bde47200f53cff4f7932e604083c
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-14 13:49:36 -0800

    feat: orders seed started

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -1,15 +1,15 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 module.exports = db.define('cart', {
   quantity: {
     type: Sequelize.INTEGER,
     validate: {
-      min: 0
-    }
+      min: 0,
+    },
   },
   //   product quantity * unit price
   totalPriceForThisProduct: {
-    type: Sequelize.DECIMAL
-  }
-})
+    type: Sequelize.DECIMAL(10, 2),
+  },
+});
