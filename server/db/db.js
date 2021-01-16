92c516243e554ad8f9cac12925c353818a5c1de1
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-16 14:57:44 -0500

    fixed prettier, deleted abbie's file, changed deploy script to checkout current branch when complete, and deploy main instead of master. Uncommented local db in db.js, so both heroku and local work

--- server/db/db.js
+++ server/db/db.js
@@ -6,23 +6,23 @@ const databaseName =
 
 let db;
 
-// if (process.env.DATABASE_URL) {
-//heroku configuration
-db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false,
-  dialect: 'postgres',
-  protocol: 'postgres',
-  ssl: true,
-  dialectOptions: {
+if (process.env.DATABASE_URL) {
+  //heroku configuration
+  db = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
     ssl: true,
-  },
-});
-// } else {
-//   //local configuration
-//   db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
-//     logging: false,
-//   });
-// }
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+} else {
+  //local configuration
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+    logging: false,
+  });
+}
 module.exports = db;
 
 // This is a global Mocha hook used for resource cleanup.
