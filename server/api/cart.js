727df836fbb7f0dda30abfed2a99079aaaf38801
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-01-15 16:06:13 -0800

    need to commit to change branch

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,7 +7,7 @@ const Cart = require('../db/models/cart');
 router.get('/', async (req, res, next) => {
   try {
     const allCarts = await Cart.findAll({
-      attributes: ['quantity', 'totalPriceForThisProduct', 'orderId']
+      // attributes: ['quantity', 'totalPriceForThisProduct', 'orderId'],
     });
     res.send(allCarts);
   } catch (error) {
@@ -18,8 +18,11 @@ router.get('/', async (req, res, next) => {
 router.get('/:cartId', async (req, res, next) => {
   try {
     // look up eager loading for many to many relationship
+    // look into model class and instance methods?
+
     const thisCart = await Cart.findAll({
       where: {
+        // should be able to chagne to findByPk because Hillary added PK to cart table
         orderId: req.params.cartId
       }
       //   include: {
