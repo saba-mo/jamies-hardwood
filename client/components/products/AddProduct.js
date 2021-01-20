7bca00f95fa53d1996b593394d3586ef95413ca0
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-19 23:19:50 -0500

    fully functioning add-product button with admin only access

--- client/components/products/AddProduct.js
+++ client/components/products/AddProduct.js
@@ -2,51 +2,71 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createProduct} from '../../store/redux/products/products';
 
-const AddProduct = (props) => {
-  const {handleSubmit} = props;
+const defaultState = {
+  name: '',
+  description: '',
+};
+
+class AddProduct extends React.Component {
+  constructor() {
+    super();
+    this.state = defaultState;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   //name, description, quantity, price, image
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    try {
+      this.props.addProduct(this.state.name, this.state.description);
+      this.setState(defaultState);
+    } catch (error) {
+      console.log('I failed to handle submission!', error);
+    }
+  }
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">
-            <small>Product Name</small>
-          </label>
-          <input name="name" type="text" />
-        </div>
-        <div>
-          <label htmlFor="description">
-            <small>Description</small>
-          </label>
-          <input name="description" type="text" />
-        </div>
-        <div>
-          <button type="submit">Add Product</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+  render() {
+    const {name, description} = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name">
+              <small>Product Name</small>
+            </label>
+            <input
+              name="name"
+              type="text"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">
+              <small>Description</small>
+            </label>
+            <input
+              name="description"
+              type="text"
+              value={description}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Add Product</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
-const mapDispatch = (dispatch) => {
-  return {
-    handleSubmit(evt) {
-      evt.preventDefault();
-      const name = evt.target.name.value;
-      const description = evt.target.description.value;
-      dispatch(createProduct(name, description));
-    },
-  };
-};
+const mapDispatch = (dispatch) => ({
+  addProduct: (name, description) => dispatch(createProduct(name, description)),
+});
 
 export default connect(null, mapDispatch)(AddProduct);
-
-/**
- * PROP TYPES
- */
-// AddProduct.propTypes = {
-//   handleSubmit: PropTypes.func.isRequired,
-// error: PropTypes.object,
-// };
