be3a2566e29e4ff2534fdecbfe42795a51ab83b1
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-20 12:15:07 -0800

    feat: restructure navbar permissions

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,71 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
 
-const Navbar = ({handleClick, isLoggedIn, isAdmin, orderId}) => {
-  if (isAdmin && isLoggedIn) {
+class Navbar extends Component {
+  // const Navbar = (handleClick, isLoggedIn, isAdmin, orderId) => {
+
+  render() {
+    const {handleClick, isLoggedIn, isAdmin, orderId} = this.props;
     return (
       <div>
         <h1>Jamie's Hardwood and Heavy Metal</h1>
         <nav>
           <div>
-            {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
             <Link to="/products">Shop</Link>
-            <Link to={`/cart/${orderId}`}>Cart</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/addproduct">Add Product</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
+            {isAdmin && isLoggedIn && <Link to="/addproduct">Add Product</Link>}
+            {isAdmin && isLoggedIn && (
+              <Link to="/users" className="admin-nav">
+                Users
+              </Link>
+            )}
+            {isLoggedIn && <Link to={`/cart/${orderId}`}>Cart</Link>}
+            {isLoggedIn && (
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            )}
+            {!isLoggedIn && <Link to="/signup">Sign Up</Link>}
+            {!isLoggedIn && <Link to="/home">Sign In</Link>}
           </div>
         </nav>
-        <hr />
-      </div>
-    );
-  } else if (isLoggedIn) {
-    return (
-      <div>
-        <h1>Jamie's Hardwood and Heavy Metal</h1>
-        <nav>
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <Link to="/products">Shop</Link>
-            <Link to={`/cart/${orderId}`}>Cart</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-        </nav>
-        <hr />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Jamie's Hardwood and Heavy Metal</h1>
-        <nav>
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <Link to="/products">Shop</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        </nav>
-        <hr />
       </div>
     );
   }
-};
+}
 
-/**
- * CONTAINER
- */
+/* * CONTAINER * */
+
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.user.id,
@@ -84,9 +57,8 @@ const mapDispatch = (dispatch) => {
 
 export default connect(mapState, mapDispatch)(Navbar);
 
-/**
- * PROP TYPES
- */
+/* * PROP TYPES * */
+
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
