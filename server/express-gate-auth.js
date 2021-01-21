278e7a09eca229a49f40d378063cd4d8b52b01db
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-20 21:42:31 -0500

    cart isn't accessible to other users

--- server/express-gate-auth.js
+++ server/express-gate-auth.js
@@ -1,16 +1,22 @@
+const User = require('./db/models/user');
+const Order = require('./db/models/order');
+
+//checking if the user is an admin
 const isAdmin = async (req, res, next) =>
   req.user.isAdmin ? next() : res.send("You don't have permission");
 
-//req.user comes ??? Contains an object of information about a logged in user, which allows us to access "isAdmin" etc.
-
 //checking if the user identity is the identity associated with this order
 const isIdentity = async (req, res, next) => {
-  console.log('req.user', req.user);
-  // console.log('req.params', req.params)
-  // console.log('req', req);
+  const user = await User.findOne({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      model: Order,
+    },
+  });
 
-  //req.user doesn't include Order. How do we include order?
-  req.user.order.id === req.params.cartId
+  user.order.id == req.params.cartId
     ? next()
     : res.send("You don't have permission");
 };
