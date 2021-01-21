0b500b851bce38899b10f74b6bf889d1b75900dc
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-20 21:26:08 -0800

    doc: rm console.logs

--- client/store/redux/users/usersView.js
+++ client/store/redux/users/usersView.js
@@ -53,7 +53,6 @@ const initialState = [];
 export default function (users = initialState, action) {
   switch (action.type) {
     case DELETE_USER:
-      console.log('state users', users);
       users = users.filter(
         (user) => parseInt(user.id) !== parseInt(action.user.id)
       );
@@ -64,8 +63,6 @@ export default function (users = initialState, action) {
       );
       return users.concat([action.user]);
     case GET_USERS:
-      console.log('state of users', users);
-      console.log('users after action to find all', action.users);
       return action.users;
     default:
       return users;
